0a97dd5fc31e0d53e354f275fac0bf584fed6e78
saurabh-bengani <saurabh.bengani@wissen.com>
2018-07-16 04:14:58 -0700

    Initial Commit
    Swagger Config
    Spring Boot Initial Config

---
+++ wissen-social/src/main/java/com/systems/wissen/WissenSocialApplication.java
@@ -0,0 +1,12 @@
+package com.systems.wissen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WissenSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WissenSocialApplication.class, args);
+	}
+}
