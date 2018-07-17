fe28c4864c38e310938a61cac47bc1b657b06e77
NinadPrabhukhanolkar <ninad.prabhukhanolkar@wissen.com>
2018-07-17 06:22:15 -0700

    Register Form Updates
    Added from group to address,
    changed names of attributes,
    Certification object,
    Skill objects created,
    Gender, Marital status, Password added

--- SocialWeb/src/app/register/register.component.ts
+++ SocialWeb/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormArray,FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,59 +8,81 @@ import { FormGroup, FormBuilder, Validators, FormArray,FormControl } from '@angu
 })
 export class RegisterComponent implements OnInit {
 
-  registerForm:FormGroup;
+  registerForm: FormGroup;
   //skills:any=[];
-  
-  constructor(private fb:FormBuilder) { }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.registerForm=this.fb.group({
-      empId:['',Validators.required],
-      firstName:['',Validators.required],
-      lastName:[''],
-      email:['',[Validators.required,Validators.email]],
-      dob:['',Validators.required],
-      mobile:[''],
-      workPhone:[''],
-      managerId:[''],
-      currentPosition:[''],
-      street:[''],
-      city:[''],
-      state:[''],
-      country:[''],
-      zip:[''],
-      education:[''],
-      institutionName:[''],
-      bio:[''],
-      skills:this.fb.array([new FormControl('')]),
-      certificates:this.fb.array([new FormControl('')]),
-      resume:['']
-      
+    this.registerForm = this.fb.group({
+      empId: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: [''],
+      bioPic:[''],
+      gender:['Male'],
+      password:[''],
+      confirmPassword:[''],
+      maritalStatus:['Unmarried'],
+      emailId: ['', [Validators.required, Validators.email]],
+      dateOfBirth: ['', Validators.required],
+      contactNumberPersonal: [''],
+      contactNumberWork: [''],
+      managerId: [''],
+      currentPosition: [''],
+      address: this.fb.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipcode: [''],
+      }),
+      qualificationDegree: [''],
+      instituteName: [''],
+      bio: [''],
+      skills: this.fb.array([this.createSkill()]),
+      certifications: this.fb.array([this.createCertificate()]),
+      resume: ['']
+
 
       // skills:this.fb.array([this.createSkill()])
     })
   }
+
+  createSkill(){
+    return this.fb.group({
+      allSkillId:''
+    });
+  }
+
   addSkill() {
-    (<FormArray>this.registerForm.get('skills')).push(new FormControl(''));
-   }
+    (<FormArray>this.registerForm.get('skills')).push(this.createSkill());
+  }
 
-   removeSkill(i: number) {
+  removeSkill(i: number) {
     (<FormArray>this.registerForm.get('skills')).removeAt(i);
-   }
+  }
 
-   addCertificate(){
-    (<FormArray>this.registerForm.get('certificates')).push(new FormControl(''));
-   }
+  createCertificate() {
+    return this.fb.group({
+      completionYear: '',
+      certificationName: ''
+    });
+  }
 
-   removeCertificate(i: number) {
-    (<FormArray>this.registerForm.get('certificates')).removeAt(i);
-   }
+  addCertificate() {
+    (<FormArray>this.registerForm.get('certifications')).push(this.createCertificate());
+  }
 
-   register(){
-     console.log(this.registerForm.value)
-   }
+  removeCertificate(i: number) {
+    (<FormArray>this.registerForm.get('certifications')).removeAt(i);
+  }
 
- 
+  register(e) {
+    e.preventDefault();
+    console.log(JSON.stringify(this.registerForm.value))
+  }
+
+
 
 
 }
