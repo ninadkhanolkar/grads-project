c69c479f6f7ff4dc72ad77a212c740609294ba98
saurabh-bengani <saurabh.bengani@wissen.com>
2018-07-19 18:48:19 +0530

    last fix

--- wissen-social/src/main/java/com/systems/wissen/model/Employee.java
+++ wissen-social/src/main/java/com/systems/wissen/model/Employee.java
@@ -89,7 +89,7 @@ public class Employee implements Serializable {
 	}
 
 	public void setManagerId() {
-		if(employee.emailId != null)
+		if(employee.empId != null)
 		this.managerId = this.employee.empId;
 		else 
 			this.managerId="";
