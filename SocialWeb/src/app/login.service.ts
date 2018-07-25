2292f402341ec76e9a8d895e6655fcb9ad134f1e
NinadPrabhukhanolkar <ninad.prabhukhanolkar@wissen.com>
2018-07-25 15:59:04 +0530

    Login service

--- SocialWeb/src/app/login.service.ts
+++ SocialWeb/src/app/login.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -8,16 +9,27 @@ export class LoginService {
 
   isAuthenticated:boolean=false;
   token:any;
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient,private router:Router) { }
    
-  getToken(credential){
+  tryLogin(credential,userRole){
+    this.token="";
+    this.isAuthenticated=false;
     let url = "http://localhost:8080/auth";
     this.http.post(url,credential).subscribe((e:any)=>{
+      console.log(e);
       if(e["token"]){
       this.token=e["token"];
       this.isAuthenticated=true;
+      if (userRole === 'Admin') {
+          this.router.navigate(['admin/profile', { 'role': userRole, 'username': credential.username }]);
+        }
+        if (userRole === 'Employee') {
+          this.router.navigate(['employee/profile', { 'role': userRole, 'username': credential.username }]);
+        }
+      
       }
     })
+    return true;
 
   }
 
