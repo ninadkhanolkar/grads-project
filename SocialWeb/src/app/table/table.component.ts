5dd2cb185e78b00d4427dab9f90b6138e014f78f
SUBHAM SAHAY <subhamkumar.sahay2014@vit.ac.in>
2018-07-26 11:02:49 +0530

    search functionality in ui

--- SocialWeb/src/app/table/table.component.ts
+++ SocialWeb/src/app/table/table.component.ts
@@ -1,35 +1,52 @@
-import {Component, OnInit, Input} from '@angular/core';
-import {EmployeeService} from '../employee.service';
-import {Router, ActivatedRoute} from '@angular/router';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { EmployeeService } from '../employee.service';
+import { Router, ActivatedRoute } from '@angular/router';
 import { LoginService } from '../login.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
-export class TableComponent implements OnInit {
+export class TableComponent implements OnInit,OnDestroy {
 
-  id:String;
+  id: String;
   employeeCredentials: any = [];
-  type:String;
+  type: String;
+  employees:{firstName:string,lastName:string,id:string}[];
+  filteredEmployees:any=[];
+  subscription:Subscription;
 
   constructor(private employeeService: EmployeeService,
-              private router: Router,
-              private route:ActivatedRoute,
-              private loginService:LoginService) {}
+    private router: Router,
+    private route: ActivatedRoute,
+    private loginService: LoginService) {
+  //  this.subscription=this.employeeService.loadReportees(this.id).subscribe(employees =>this.filteredEmployees= this.employees = employees);
+  }
 
+
+  filter(query: string) {
+    this.filteredEmployees=(query) ?
+    this.employeeCredentials.filter(e=>e.firstName.toLowerCase().includes(query.toLowerCase())):
+    this.employeeCredentials;
+  }
+
+  ngOnDestroy(){
+   this.subscription.unsubscribe();
+  }
+  
   ngOnInit() {
-    this.type=this.route.snapshot.paramMap.get('type');
-    if(this.type ==='employees'){
+    this.type = this.route.snapshot.paramMap.get('type');
+    if (this.type === 'employees') {
       this.getCredentials();
     }
-    else if(this.type==='reportees'){
-      this.id=this.loginService.username;
+    else if (this.type === 'reportees') {
+      this.id = this.loginService.username;
       console.log(this.id)
       this.getReportees(this.id);
     }
-    
+
   }
 
   getCredentials() {
@@ -40,9 +57,12 @@ export class TableComponent implements OnInit {
 
   }
 
-  getReportees(id){
-    this.employeeService.loadReportees(id).subscribe((credentials)=>{
-      this.employeeCredentials=credentials;
+
+
+  getReportees(id) {
+   this.subscription= this.employeeService.loadReportees(id).subscribe((credentials) => {
+     this.filteredEmployees= this.employeeCredentials = credentials;
+     console.log(this.employeeCredentials);
     })
   }
 
